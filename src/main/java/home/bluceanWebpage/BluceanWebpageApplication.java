84427e1b86b490f1d754a3423cf520ca6b30450e
booyong <booyong@blucean.net>
2023-11-20 15:29:16 +0900

    first commit

---
+++ src/main/java/home/bluceanWebpage/BluceanWebpageApplication.java
@@ -0,0 +1,13 @@
+package home.bluceanWebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluceanWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluceanWebpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/bluceanWebpage/BluceanWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package home.bluceanWebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluceanWebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
