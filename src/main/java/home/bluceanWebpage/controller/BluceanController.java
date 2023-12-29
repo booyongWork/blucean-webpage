bc911845a57ef71b99558f21d03f3f74be604b95
booyong <booyong@blucean.net>
2023-12-29 12:45:43 +0900

    - 플랫폼 운영 페이지 자동 롤링 클릭 슬라이더로 변경

--- src/main/java/home/bluceanWebpage/controller/BluceanController.java
+++ src/main/java/home/bluceanWebpage/controller/BluceanController.java
@@ -1,29 +1,47 @@
 package home.bluceanWebpage.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Slf4j
 @Controller
 public class BluceanController {
+
+    /**
+     * init 페이지
+     *
+     */
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "views/hello";
     }
 
+    /**
+     * 플랫폼 개발 페이지
+     *
+     */
     @GetMapping("platformDev")
     public String platformDev() {
 
         return "views/platformDev";
     }
 
+    /**
+     * 플랫폼 운영 페이지
+     *
+     */
     @GetMapping("platformOps")
     public String platformOps() {
 
         return "views/platformOps";
     }
 
+    /**
+     * 솔루션 페이지
+     *
+     */
     @GetMapping("solutionDev")
     public String solutionDev() {
 
