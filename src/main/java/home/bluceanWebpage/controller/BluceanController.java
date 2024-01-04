ee81baea569214fbac812a7c79bbdfbc68a86b72
moon <u_u2222@blucean.net>
2024-01-04 10:13:09 +0900

    - lombok 적용 - 플랫폼 개발 최신순 과거순 추가

--- src/main/java/home/bluceanWebpage/controller/BluceanController.java
+++ src/main/java/home/bluceanWebpage/controller/BluceanController.java
@@ -32,19 +32,19 @@ public class BluceanController {
     @GetMapping("platformDev")
     public String platformDev(Model model) {
         List<Business> businessList = new ArrayList<>();
-        businessList.add(new Business("/img/businessLogo/img_s1_01.png", "현대자동차", "현대자동차 중고차 E-Commerce 개발"));
-        businessList.add(new Business("/img/businessLogo/img_s1_02.png", "신세계", "신세계 I&C API 플랫폼 개발"));
-        businessList.add(new Business("/img/businessLogo/img_s1_03.png", "한국투자", "한국투자증권 리뉴얼"));
-        businessList.add(new Business("/img/businessLogo/img_s1_04.png", "대신증권", "대신증권 마이데이터 시스템 구축"));
-        businessList.add(new Business("/img/businessLogo/img_s1_05.png", "kt alpha", "쇼핑 E-Commerce 차세대 시스템 개발"));
-        businessList.add(new Business("/img/businessLogo/img_s1_07.png", "seezn", "KT OTT 플랫폼(시즌) 추천/검색 시스템 구축"));
-        businessList.add(new Business("/img/businessLogo/img_s1_08.png", "whowho", "Biz WhoWho 차세대 플랫폼 구축"));
-        businessList.add(new Business("/img/businessLogo/img_s1_11.png", "kt ds", "RCS (Rich Contents Service) 허브 플랫폼 구축"));
-        businessList.add(new Business("/img/businessLogo/img_s1_13.png", "단국대학교", "단국대학교 교육지원 시스템 및 서비스 개발"));
-        businessList.add(new Business("/img/businessLogo/img_s1_17.png", "kbank", "케이뱅크 API 플랫폼 고도화 개발"));
-        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "LS ITC", "LS ITC 전기차 관제플랫폼 개발"));
-//        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "TEST", "LS ITC 전기차 관제플랫폼 개발"));
-//        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "TEST", "LS ITC 전기차 관제플랫폼 개발"));
+        businessList.add(new Business("/img/businessLogo/img_s1_01.png", "현대자동차", "현대자동차 중고차 E-Commerce 개발", "2001~2022년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_02.png", "신세계", "신세계 I&C API 플랫폼 개발", "2002년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_03.png", "한국투자", "한국투자증권 리뉴얼", "2003년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_04.png", "대신증권", "대신증권 마이데이터 시스템 구축", "2004년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_05.png", "kt alpha", "쇼핑 E-Commerce 차세대 시스템 개발", "2005년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_07.png", "seezn", "KT OTT 플랫폼(시즌) 추천/검색 시스템 구축", "2006년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_08.png", "whowho", "Biz WhoWho 차세대 플랫폼 구축", "2007년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_11.png", "kt ds", "RCS (Rich Contents Service) 허브 플랫폼 구축", "2008년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_13.png", "단국대학교", "단국대학교 교육지원 시스템 및 서비스 개발", "2009년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_17.png", "kbank", "케이뱅크 API 플랫폼 고도화 개발", "2010년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "LS ITC", "LS ITC 전기차 관제플랫폼 개발", "2011년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "TEST", "LS ITC 전기차 관제플랫폼 개발", "2012년"));
+        businessList.add(new Business("/img/businessLogo/img_s1_18.png", "TEST", "LS ITC 전기차 관제플랫폼 개발", "2013년"));
 
         model.addAttribute("businessList", businessList);
         return "views/platformDev";
